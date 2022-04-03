f93a5b6487c25d7af742155de41ff0dfebeccc3a
yoshito-tasaki <100545845+yoshito-tasaki@users.noreply.github.com>
2022-04-04 00:28:08 +0900

    Create database.js

---
+++ database.js
@@ -0,0 +1,25 @@
+const sdGoals1 = [
+    "募金した"
+]
+
+const sdGoals2 = [
+    "食べたものを残さない",
+    "賞味期限が近いものを購入した"
+]
+
+const sdGoals3 = [
+    "献血を行った",
+    "介護教室へ参加した"
+]
+
+const sdGoals4 = [
+    "新しいオンデマンド講座を開発した",
+    "取り組みが加速しているマイコン講座を開講した"
+]
+
+const sdGoals5 = [
+    "夫婦間で積極的に家事を分担している",
+    "重量物作業を低減した"
+]
+
+const sdGoalsAll = [sdGoals1,sdGoals2,sdGoals3,sdGoals4,sdGoals5];
