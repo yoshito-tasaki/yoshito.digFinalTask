f078a8bf50766a90db2370ed79c03c8ea682d2e1
yoshito-tasaki <100545845+yoshito-tasaki@users.noreply.github.com>
2022-04-04 17:05:01 +0900

    Update database.js

--- database.js
+++ database.js
@@ -19,7 +19,70 @@ const sdGoals4 = [
 
 const sdGoals5 = [
     "夫婦間で積極的に家事を分担している",
-    "重量物作業を低減した"
+    "重量物作業を低減した",
+    "育休を取得した"
 ]
 
-const sdGoalsAll = [sdGoals1,sdGoals2,sdGoals3,sdGoals4,sdGoals5];
+const sdGoals6 = [
+    "自治会の河川美化活動に参加"
+]
+
+const sdGoals7 = [
+    "自宅にソーラーパネルを設置した",
+    "自宅に蓄電池を設置した"
+]
+
+const sdGoals8 = [
+    "SDGsに積極的に取組んでいる企業の製品を使用",
+    "トヨタイムズ視聴"
+]
+
+const sdGoals9 = [
+    "災害復興ボランティアに参加",
+    "災害復旧に対して募金"
+]
+
+const sdGoals10 = [
+    "手話を学んでいる",
+    "ユニバーサルデザインな備品の導入"
+]
+
+const sdGoals11 = [
+    "地域の清掃活動に参加",
+    "犬の散歩中にゴミ拾いをしている"
+]
+
+const sdGoals12 = [
+    "子供服、おもちゃのリサイクル",
+    "リサイクル製品の積極的利用"
+]
+
+const sdGoals13 = [
+    "節電を実施",
+    "エレベーターより階段を利用"
+]
+
+const sdGoals14 = [
+    "食堂でサスティナブルシーフードメニューを食べる",
+    "使い捨てカトラリーを使わない"
+]
+
+const sdGoals15 = [
+    "植樹活動に参加"
+]
+
+const sdGoals16 = [
+    "戦争で苦しむ方への募金",
+    "世界情勢に関するニュースを毎日見る"
+]
+
+const sdGoals17 = [
+    "関係各部との積極的な交流で業務改善"
+]
+
+const sdGoals18 = [
+    "？？？"
+]
+
+const sdGoalsAll = [sdGoals1,sdGoals2,sdGoals3,sdGoals4,sdGoals5,sdGoals6,sdGoals7,sdGoals8,sdGoals9,
+    sdGoals10,sdGoals11,sdGoals12,sdGoals13,sdGoals14,sdGoals15,sdGoals16,sdGoals17,sdGoals18];
